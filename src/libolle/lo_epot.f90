75ea523d22fd33e7aa9e6c37b3076d27a29db3c0
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2025-09-05 12:27:21 -0400

    oops

--- src/libolle/lo_epot.f90
+++ src/libolle/lo_epot.f90
@@ -60,7 +60,7 @@ module lo_epot
         type(lo_canonical_configs), intent(inout), optional :: sim
     
         type(lo_crystalstructure) :: p
-        integer :: ctr, i,
+        integer :: ctr, i
         real(r8), dimension(:, :), allocatable :: f2, f3, f4, fp
         real(r8) :: e2, e3, e4, ep, ek
         real(r8), dimension(3, 3) :: m0

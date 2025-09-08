9175b0a6aa2d9cbd3ef1bb6f33a7d98a57e3f058
Ethan Meitz <54505069+ejmeitz@users.noreply.github.com>
2025-09-08 12:13:33 -0400

    add error reporting

--- src/effective_hamiltonian/main.f90
+++ src/effective_hamiltonian/main.f90
@@ -147,12 +147,10 @@ energy : block
 
         if (opts%dumpconfigs) then
             call pot%statistical_sampling(uc, ss, fc2, opts%nconf, opts%temperature, opts%quantum, ebuf, mw, mem, opts%verbosity, cc)
+            call cc%write_hdf5_mpi(mw, 'outfile.canonical_configs.hdf5')
         else
             call pot%statistical_sampling(uc, ss, fc2, opts%nconf, opts%temperature, opts%quantum, ebuf, mw, mem, opts%verbosity)
         end if
-
-
-        call cc%write_hdf5_mpi(mw, 'outfile.canonical_configs.hdf5')
 
     else
 
